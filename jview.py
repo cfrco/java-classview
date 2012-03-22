03f4a29a0b04526928c0377c5a7238d23ea0ad43
cfrco <z82206.cat@gmail.com>
2012-03-22 22:17:12 +0800

    fix README

--- jview.py
+++ jview.py
@@ -7,6 +7,7 @@ def main():
     for arg in sys.argv[1:]:
         args += arg+" "
     
+    #edit this path for your env
     os.chdir("/home/cat/code/mygithub/java/classview")
     os.system("java ClassView "+args)
 
