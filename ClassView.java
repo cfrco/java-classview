8a578eb2db2e69dae35c0ab17cb5607f67b1904f
cfrco <z82206.cat@gmail.com>
2012-03-22 21:51:57 +0800

    default print and default package

--- ClassView.java
+++ ClassView.java
@@ -6,7 +6,26 @@ class ClassView
     {
         try
         {
-            Class<?> c = Class.forName(args[0]);
+            String[] DefaultPackage = {
+                "java.lang.",
+                "java.util."
+            };
+            Class<?> c = Class.forName("java.lang.Object");
+            int i = 0;
+            while(i<DefaultPackage.length)
+            {
+                try
+                {
+                    c = Class.forName(DefaultPackage[i]+args[0]);
+                    break;
+                }
+                catch (ClassNotFoundException e)
+                { ++i; }
+            }
+
+            if(i>=DefaultPackage.length)
+                c = Class.forName(args[0]);
+
             String classCompleteName = args[0];
             String classSimpleName = args[0].substring(args[0].lastIndexOf(".")+1);
             String classPrefixName = args[0].substring(0,args[0].lastIndexOf(".")+1);
@@ -29,11 +48,24 @@ class ClassView
                 if(args[1].equals("dfield"))
                     printMember(c.getDeclaredFields(),hiddenPrefixList);
             }
+            else
+            {
+                printAll(c,hiddenPrefixList);
+            }
         }
         catch (ClassNotFoundException e)
         {
             System.out.println("Unknown Class : "+args[0]);
         }
+    }
+    private static void printAll(Class<?> c,String[] hiddenPrefixList)
+    {
+        printMember(c.getConstructors(),hiddenPrefixList);
+        printMember(c.getDeclaredConstructors(),hiddenPrefixList);
+        printMember(c.getMethods(),hiddenPrefixList);
+        printMember(c.getDeclaredMethods(),hiddenPrefixList);
+        printMember(c.getFields(),hiddenPrefixList);
+        printMember(c.getDeclaredFields(),hiddenPrefixList);
     }
     private static void printMember(Member[] mems,String[] hiddenPrefixList)
     {
